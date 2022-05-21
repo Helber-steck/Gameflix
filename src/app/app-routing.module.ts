d20f0ef7fd9be75881d7fd950e20d28c53880814
Helber-Steck <helber4255@yhaoo.com.br>
2022-05-21 09:29:39 -0300

    adicionando modulos para carregar por demanda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { PerfilComponent } from './perfil/perfil.component';
+
 
 const routes: Routes = [
-  { path: 'perfil', component: PerfilComponent },
-  { path: 'home', component: HomeComponent },
-  { path: '', pathMatch:'full', redirectTo: '/perfil' },
+  { 
+    path: 'perfil', 
+    loadChildren: () => import('./perfil/perfil.module').then(m => m.PerfilModule)
+  },
+
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '',
+    pathMatch:'full',
+    redirectTo: '/perfil'
+  }
 ];
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
-  ],
+    RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
